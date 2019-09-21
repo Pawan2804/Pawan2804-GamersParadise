54eaba810694eb7556302999d63d5fbcb0160178
Pawan.V <42887444+Pawan2804@users.noreply.github.com>
2019-09-21 11:21:01 +0530

    login page done

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login For The Gamer In You </title>
+	<link rel="stylesheet" type="text/css" href="LoginStyle.css">
+<!-- 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+</head>
+<body>
+<div class="login-box">
+	<h1>Login</h1>
+	<div class="textbox">
+	<i class="fa fa-user" aria-hidden="true"></i>
+	<input type="text" placeholder="User Name" name="username">
+	</div>
+	<div class="textbox">
+	<i class="fa fa-lock" aria-hidden="true"></i>
+	<input type="password" placeholder="Password" name="password">	
+	</div>
+	<input class="btn" type="button" name="submit" value="Login">
+</div>
+</div>
+</body>
+</html>
